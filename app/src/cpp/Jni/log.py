7d66bc83343d30081de4dfa367a466c1f8490394
ninjacoder <89408667+Ninjacoderhsi@users.noreply.github.com>
2022-02-25 00:43:04 +0330

    Add files via upload

---
+++ app/src/cpp/Jni/log.py
@@ -0,0 +1,20 @@
+import requests
+import pyjava
+
+
+if pyjava.keyword_only :
+    for a in range(90):
+        print("Error")
+else:
+    pass
+    
+    
+    
+def Icolent(self):
+    return self
+    
+    print(self)
+    
+    
+Icolent(divmod)
+    
