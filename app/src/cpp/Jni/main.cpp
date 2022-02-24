e9c59982669ec18d54c5205ed8ec3c4fe39057e3
ninjacoder <89408667+Ninjacoderhsi@users.noreply.github.com>
2022-02-25 00:29:54 +0330

    Add file main.cpp

---
+++ app/src/cpp/Jni/main.cpp
@@ -0,0 +1,23 @@
+#include <iosteram>
+#include <String>
+#include <path>
+using namespace std;
+
+{ 
+     int a = 800;
+     int b = 8001;
+     bool = true;
+     
+     if (iosteram.std > = path){
+        std.cin >> "null obj" >> add.method.lockal();
+        try{ 
+          cin.getOUTpath("/sdcard/Approid")
+           
+            } catch(IOException io){
+            
+            cout << io ;
+            return io;
+            }
+          }else{
+             return > std;
+             }
